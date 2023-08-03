19de8c578a5a7d89d8d3245e424d55be0c10299c
SusanaaMuniz20 <susana.andrade.muniz@gmail.com>
2023-08-03 17:10:26 -0300

    Criando a pasta dos projetos Java

---
+++ Atividade1.1/src/Atividade1_1/Atividade1_1.java
@@ -0,0 +1,28 @@
+package Atividade1_1;
+
+public class Atividade1_1 {
+
+	int numero1 = 10;
+	double numero2 = 20;
+	char opcao = 's';
+	String mensagem ="Boa Tarde!";
+	boolean valor = true;
+	
+	String nome = "Susana";      
+    String sobrenome ="Muniz";
+    
+	System.out.println(numero1);
+	System.out.println(numero2);
+	System.out.println(opcao);
+	System.out.println(mensagem);
+
+	System.out.println("Nome completo: " + nome + " " + sobrenome);
+	
+			
+	// inicio da tividade 2
+	
+	
+	
+	
+	
+}
